c29c444cd423e4072f2247c0bded9608c28d61c0
marianaalvarezvillalta <marianaalv07@gmail.com>
2022-10-19 20:06:42 -0600

    clases para pilas

--- ExamenI_Grupo6/src/sodaFide/Cola.java
+++ ExamenI_Grupo6/src/sodaFide/Cola.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package sodaFide;
 
-/**
- *
- * @author Wstov
- */
 public class Cola {
     private NodoCola frente;
     private NodoCola ultimo;
---
+++ ExamenI_Grupo6/src/sodaFide/NodoPila.java
@@ -0,0 +1,5 @@
+package sodaFide;
+
+public class NodoPila {
+    
+}
