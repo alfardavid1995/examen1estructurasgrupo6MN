d021ba9a108583fdff4ad5de5ef44afa7b82a65e
marianaalvarezvillalta <marianaalv07@gmail.com>
2022-10-19 21:00:22 -0600

    clase NodoPila

--- ExamenI_Grupo6/src/sodaFide/NodoPila.java
+++ ExamenI_Grupo6/src/sodaFide/NodoPila.java
@@ -1,5 +1,29 @@
 package sodaFide;
 
 public class NodoPila {
+    private Traste trasteNodo;
+    private NodoPila siguiente;
     
+    //constructor para inicializar
+
+    public NodoPila() {
+        this.trasteNodo = null;
+        this.siguiente = null;
+    }
+
+    public Traste getTrasteNodo() {
+        return trasteNodo;
+    }
+
+    public void setTrasteNodo(Traste trastecitoNodo) {
+        this.trasteNodo = trastecitoNodo;
+    }
+
+    public NodoPila getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(NodoPila siguiente) {
+        this.siguiente = siguiente;
+    }
 }
