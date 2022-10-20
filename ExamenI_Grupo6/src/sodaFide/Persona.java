214ab970dbb58205c2d97dc58ad3687734b9c681
marianaalvarezvillalta <marianaalv07@gmail.com>
2022-10-19 23:36:58 -0600

    correccion de margen3

--- ExamenI_Grupo6/src/sodaFide/Persona.java
+++ ExamenI_Grupo6/src/sodaFide/Persona.java
@@ -39,7 +39,8 @@ public class Persona {
 
     @Override
     public String toString() {
-        return "Persona{" + "nombre=" + nombre + ", cedula=" + cedula + ", discapacidad=" + discapacidad + '}';
+        return "Persona{" + "nombre=" + nombre + ", cedula=" + cedula 
+                + ", discapacidad=" + discapacidad + '}';
     }
     
 }
